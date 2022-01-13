7e2e0188e223c55c220e63feadd0447d69850f9b
Hilda Diana Cardenas <hcardenas00@gmail.com>
2022-01-13 08:17:35 -0500

    code along complete with notes

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+  rescue ActiveRecord::RecordNotFound, with :render_not_found_response #(this rescue will run if any of our controller actions into that error )
 
   # GET /birds
   def index
@@ -12,53 +13,103 @@ class BirdsController < ApplicationController
     render json: bird, status: :created
   end
 
+
   # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
+    # OLD GET -V
+    # def show
+    #   find_bird
+    #   if bird
+    #     render json: bird
+    #   else
+    #     render_not_found_response
+    #   end
+    # end
+
+    #NEW GET -V:
+    def show
+      find_bird
       render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
+      
+    #Don't need it anymore. Handled by rescue above ^
+      # rescue ActiveRecord::RecordNotFound
+    #     render_not_found_response
     end
-  end
 
   # PATCH /birds/:id
-  def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
+    #OLD PATCH
+    # def update
+    #   find_bird
+    #   if bird
+    #     bird.update(bird_params)
+    #     render json: bird
+    #   else
+    #     render_not_found_response
+    #   end
+    # end
+
+    #NEW PATCH -V 
+    def update
+      find_bird
       bird.update(bird_params)
       render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
+    
+    #Don't need it anymore. Handled by rescue above ^  
+    # rescue ActiveRecord::RecordNotFound
+    #     render_not_found_response
     end
-  end
+
 
   # PATCH /birds/:id/like
   def increment_likes
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(likes: bird.likes + 1)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    find_bird
+    bird.update(likes: bird.likes + 1)
+    render json: bird
+    
+    # rescue ActiveRecord::RecordNotFound
+    #     render_not_found_response
+    # end
   end
 
   # DELETE /birds/:id
-  def destroy
-    bird = Bird.find_by(id: params[:id])
-    if bird
+  #OLD DELETE -v
+  # def destroy
+    #   find_bird
+    #   if bird
+    #     bird.destroy
+    #     head :no_content
+    #   else
+    #     render_not_found_response
+    #   end
+    # end
+    
+  # NEW DELETE -V
+    def destroy
+      find_bird
       bird.destroy
       head :no_content
-    else
-      render json: { error: "Bird not found" }, status: :not_found
+    
+    #Don't need it anymore. Handled by rescue above ^
+    # rescue ActiveRecord:: RecordNotFound #throwing an exception of this type  
+    #     render_not_found_response
     end
-  end
 
   private
 
   def bird_params
     params.permit(:name, :species, :likes)
+  end
+
+  #Placing these two under private. Anything that doesn't responde to a route is typically made private.
+  # Refactoring
+  def find_bird
+    bird = Bird.find(params[:id])
+    # bird = Bird.find_by(id: params[:id])
+  end
+
+  # Refactoring: 
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
+  
   end
 
 end
